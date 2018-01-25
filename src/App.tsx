607661d7e04ad5d006ff6cf1a069e5aa076a1648
tanbowensg <tanbowensg@gmail.com>
2018-01-25 17:51:55 +0800

    添加 BstNode 组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,14 @@
 import * as React from 'react';
+import BstNode from './components/bst/bst-node/bst-node';
 import './App.css';
-
-const logo = require('./logo.svg');
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+      <div>
+        <BstNode />
+        <BstNode />
+        <BstNode />
       </div>
     );
   }
---
+++ src/components/bst/bst-node/bst-node.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import './bst-node.css';
+
+class BstNode extends React.Component {
+  render() {
+    return (
+      <div className="bst-node">
+        5
+      </div>
+    );
+  }
+}
+
+export default BstNode;
